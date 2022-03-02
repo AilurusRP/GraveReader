969e55a33e23fb64b9751b2abf68b124368c74cd
AilurusRedPanda <1148648777@qq.com>
2022-03-02 18:53:08 +0800

    in development

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use epub::doc::EpubDoc;
+use std::io;
 
 fn main() {
-    let mut doc = EpubDoc::new("jsFp.epub").unwrap();
-    for each in doc.spine.iter() {
-        println!("{:?}", doc.resources.get(each).unwrap());
+    let mut doc = EpubDoc::new("test.epub").unwrap();
+    loop {
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer).unwrap();
+        match &buffer[..] {
+            "next()\r\n" | "next()\n" => doc.go_next().unwrap(),
+            "show()\r\n" | "show()\n" => println!("{}", doc.get_current_str().unwrap()),
+            "exit()\r\n" | "exit()\n" => break,
+            _ => (),
+
+        }
     }
-    println!("{}", doc.get_resource_str_by_path("titlepage.xhtml").unwrap());
 }
